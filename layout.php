366b95954d1d0403c81e11fc96ae7e9987ef5b1e
Ealhad <34lh4d@gmail.com>
2015-08-06 19:13:24 +0200

    fix page présentation des événements
    ça va, j'ai le truc là.

--- layout.php
+++ layout.php
@@ -1,8 +1,8 @@
 <?php
-/***************************************************
-* Script contenant le header et le footer et incluant les différentes pages
-* en fonction de la demande et de l'analyse du script d'index.
-***************************************************/
+/* * *************************************************
+ * Script contenant le header et le footer et incluant les différentes pages
+ * en fonction de la demande et de l'analyse du script d'index.
+ * ************************************************* */
 
 global $titre_page;
 global $nom_module;
@@ -17,7 +17,7 @@ global $authentification;
 <html>
     <head>
         <meta charset="utf-8" />
-	<link rel="shortcut icon" type="image/png" href="commun/img/logo_aedi_2015.png" >
+        <link rel="shortcut icon" type="image/png" href="commun/img/logo_aedi_2015.png" >
         <?php
         inclure_fichier('commun', 'bootstrap.min', 'css');
         inclure_fichier('commun', 'style', 'css');
@@ -29,45 +29,31 @@ global $authentification;
         inclure_fichier('commun', 'datepicker.fr', 'js');
         inclure_fichier('commun', 'json2', 'js');
         inclure_fichier('commun', 'utils', 'js');
-	inclure_fichier('commun', 'jquery.tablesorter.min', 'js');
-		
-		
-	inclure_fichier('commun', 'login', 'js');
-	inclure_fichier('commun', 'SHA1', 'js');
+        inclure_fichier('commun', 'jquery.tablesorter.min', 'js');
+
+
+        inclure_fichier('commun', 'login', 'js');
+        inclure_fichier('commun', 'SHA1', 'js');
         ?>
 
         <title>AEDI - <?php echo $titre_page; ?></title>
     </head>
 
     <body>
-        <?php 
-	inclure_fichier('', 'menu', 'php'); 
-	inclure_fichier('', 'login', 'php' ); 
+        <?php
+        inclure_fichier('', 'menu', 'php');
+        inclure_fichier('', 'login', 'php');
+        if ($nom_module == 'accueil') {
+            $nom_module = '';
+        }
+        ?>
 
-	/* Si on a un module, on affiche le breadcrumb, sinon ce n'est pas nécessaire */
-        /* "c'est quoi, ce 'breadcrumb' qui de toute façon n'apparaît jamais ? " Bonne question. */
-        if ($nom_module != '') { ?>
-            <ul class="breadcrumb" >
-                <li>
-                    <a href="index.php?page=Accueil"><i class="icon-home"></i></a> <span class="divider">/</span>
-                </li>
-				<?php
-				if ($titre_module != '') { ?>
-					<li>
-						<?php echo $titre_module; ?> <span class="divider">/</span>
-					</li>
-				<?php } ?>
-                <li class="active"><?php echo $titre_page; ?></li>
-            </ul>
-        <?php }
-		if ($nom_module == 'accueil') { $nom_module = ''; } ?>
-
-            <div class="module">   
-                <?php inclure_fichier($nom_module, $nom_page, 'php'); ?>
-            </div>
-        
-            <div id="layout" class="footer">&copy; AEDI - 2015</div>
+        <div class="module">   
+<?php inclure_fichier($nom_module, $nom_page, 'php'); ?>
         </div>
 
-    </body>
+        <div id="layout" class="footer">&copy; AEDI - 2015</div>
+    </div>
+
+</body>
 </html>
