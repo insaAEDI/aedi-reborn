490a1024c85553703233c15be0820b76afd00dde
Ealhad <34lh4d@gmail.com>
2015-08-06 19:32:46 +0200

    fix page RIFs

--- cvtheque/php/presentationTemp.php
+++ cvtheque/php/presentationTemp.php
@@ -11,31 +11,24 @@
 ?>
 
 <div id="cvtheque" class="container">
-	<div class="row">
-		<div id="presentationRif" class="col-sm-12">
-			<div class="well">
-				<h1>CVthèque (projet)</h1>
-				<p>
-				  <br/>
-					Dynamique et innovante, notre association cherche toujours à améliorer sa gamme de service à destination des étudiants et entreprises.<br/>
-					Nous sommes ainsi actuellement en train de réfléchir à la mise en place d'une CVthèque représentant tous les étudiants volontaires du Département. Une telle offre offrirait aux étudiants un gain en visibilité indéniable, tandis que les entreprises recevront l'opportunité d'étudier de plus près les profils intéressants.<br/>
-				</p>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-sm-4">
-			<p class="centre" style="font-size:10em;">
-				<br/><br/><br/>?
-			</p>
-			<p>
-		</div>
-		<div id="infoRif" class="col-sm-8">
-			<h2><i class="icon icon-search icon-white"></i> Nous aider</h2>
-			
-			<p>Ce projet est encore en phase d'étude. Plusieurs formules et formats ont déjà été proposés, et nous étudions l'intérêt de chacun. Néanmoins, en tant qu'acteurs potentiels de cette offre, votre avis nous intéresse beaucoup.</p>
-			<p>Nous serons ainsi heureux de pouvoir prendre en compte vos suggestions, émises à l'adresse suivante :</p>
-			<p class="centre"><a href="#" e-name="aedi.bureau" class="e-noBot btn btn-large btn-info" e-domain="listes.insa-lyon.fr"><i class="icon-eye-open icon-white"></i> Afficher Email</a></p>
-		</div>
-	</div>
+    <div class="row">
+        <div id="presentationRif" class="col-sm-12">
+            <div class="well">
+                <h1>CVthèque (projet)</h1>
+                <p>
+                    <br/>
+                    Dynamique et innovante, notre association cherche toujours à améliorer sa gamme de service à destination des étudiants et entreprises.<br/>
+                    Nous sommes ainsi actuellement en train de réfléchir à la mise en place d'une CVthèque représentant tous les étudiants volontaires du Département. Une telle offre offrirait aux étudiants un gain en visibilité indéniable, tandis que les entreprises recevront l'opportunité d'étudier de plus près les profils intéressants.<br/>
+                </p>
+            </div>
+            <div class="well">
+
+                <h2><i class="icon icon-search icon-white"></i> Nous aider</h2>
+
+                <p>Ce projet est encore en phase d'étude. Plusieurs formules et formats ont déjà été proposés, et nous étudions l'intérêt de chacun. Néanmoins, en tant qu'acteurs potentiels de cette offre, votre avis nous intéresse beaucoup.</p>
+                <p>Nous serons ainsi heureux de pouvoir prendre en compte vos suggestions, émises à l'adresse suivante :</p>
+                <p class="centre"><a href="#" e-name="aedi.bureau" class="e-noBot btn btn-large btn-info" e-domain="listes.insa-lyon.fr"><i class="icon-eye-open icon-white"></i> Afficher Email</a></p>
+            </div>
+        </div>
+    </div>
 </div>
