0ab62089c7adbdeb6db779ef1a91c635462b79ec
Aurélien Coussat <aurelien.coussat@insa-lyon.fr>
2015-10-26 16:11:45 +0100

    Mise à jour CVthèque

--- views/entreprises/cvtheque.php
+++ views/entreprises/cvtheque.php
@@ -1,19 +1,72 @@
 <div class="container">
     <div class="row">
         <div id="presentationRif" class="col-sm-12">
+
             <div class="well">
-                <h1>CVthèque (projet)</h1>
+                <h1>
+                    CVthèque
+                </h1>
                 <p>
-                    <br/>
-                    Dynamique et innovante, notre association cherche toujours à améliorer sa gamme de services à destination des étudiants et entreprises.<br/>
-                    Nous sommes ainsi actuellement en train de réfléchir à la mise en place d'une CVthèque représentant tous les étudiants volontaires du Département. Une telle offre offrirait aux étudiants un gain en visibilité indéniable, tandis que les entreprises recevront l'opportunité d'étudier de plus près les profils intéressants.<br/>
+                    La CVthèque est une bibliothèque publique de CV offrant aux étudiants une certaine visibilité tout en permettant aux entreprises d'étudier de plus près les profils les intéressant.
+                </p>
+                <p>
+                    La CVthèque prend actuellement la forme d'un dossier Google Drive en lecture seule.
+                </p>
+                <p class="text-center">
+                    <a href="https://drive.google.com/folderview?id=0B3Kliuoj7g-Lbjg1YmxZcE12cEk&amp;usp=sharing" class="e-noBot btn btn-large btn-info" target="_blank">
+                        <span class="glyphicon glyphicon-eye-open white" aria-hidden="true"></span> Accéder à la CVthèque
+                    </a>
                 </p>
             </div>
+
             <div class="well">
-                <h2><span class="glyphicon glyphicon-search white" aria-hidden="true"></span> Nous aider</h2>
-                <p>Ce projet est encore en phase d'étude. Plusieurs formules et formats ont déjà été proposés, et nous étudions l'intérêt de chacun. Néanmoins, en tant qu'acteurs potentiels de cette offre, votre avis nous intéresse beaucoup.</p>
-                <p>Nous serons ainsi heureux de pouvoir prendre en compte vos suggestions, émises à l'adresse suivante :</p>
-                <p class="centre"><a href="#" e-name="aedi.bureau" class="e-noBot btn btn-large btn-info" e-domain="listes.insa-lyon.fr"><span class="glyphicon glyphicon-eye-open white" aria-hidden="true"></span> Afficher Email</a></p>
+
+                <h2>Envoyer son CV</h2>
+
+                <p>Merci de nous envoyer votre CV en respectant les règles suivantes :
+                    <ul>
+                      <li>Le titre devra respecter le formalisme suivant : <pre>CV_ANNEE_MOIS_PRENOM_NOM_CODELANGUE.pdf</pre></li>
+                      <li><strong>Préciser votre promotion</strong> dans le mail</li>
+                      <li>Le code langue pourra être par exemple : FR, EN, ES, ainsi de suite</li>
+                      <li>Envoyer votre CV au <strong>format PDF</strong></li>
+                  </ul>
+                </p>
+
+                <p>Exemples :
+                    <pre>
+CV_2015_09_JEREMY_WIMSINGUES_EN.pdf
+CV_2015_10_JEREMY_WIMSINGUES_ES.pdf
+CV_2015_10_JULIEN_BARA_EN.pdf
+...
+                    </pre>
+                </p>
+
+                <p>
+                  Il est évident que nous aimerions éviter les mises à jour <strong>récurrentes</strong>. Merci donc de nous envoyer vos CV en <strong>version finale</strong>. 
+                  Les mises à jour ponctuelles sont évidemment possibles, il suffit pour cela de nous renvoyer un email avec votre CV à jour.
+                </p>
+
+                <p>Nous supprimerons tous les CV <strong>de plus de 6 mois</strong> afin de ne garder que les CVs les plus à jours. </p>
+
+                <p>
+                    Gardez en tête que ce dossier sera <strong>publique</strong> ! Si vous avez un problème avec la confidentialité ou la collecte des données, il est de votre responsabilité d'<strong>adapter votre CV</strong>. Nous ne sommes pas responsables de l'utilisation qui pourra être faite des CVs déposés sur le Drive.
+                </p>
+
+                <p>
+                    Merci d'envoyer votre CV à l'une des personnes suivantes si vous souhaitez participer :
+                    <ul>
+                        <li>Julien Bara (<strong>julien.bara [at] insa-lyon.fr</strong>)</li>
+                        <li>Nicolas Nadisic (<strong>nicolas.nadisic [at] insa-lyon.fr</strong>)</li>
+                        <li>Jérémy Wimsingues (<strong>jeremy.wimsingues [at] insa-lyon.fr</strong>)</li>
+                    </ul>
+                </p>
+                
+            </div>
+            <div class="well">
+                <h2>Le projet</h2>
+                <p>L'idée serait d'arriver à terme à une application hébergée directement sur ce site permettant à chacun de se connecter avec son compte INSA pour y héberger et mettre à jour son CV. Si l'idée vous intéresse et vous pensez pouvoir développer le projet (pour en faire un fil rouge par exemple), n'hésitez pas à <a href="../contact">contacter l'AEDI</a> ! Nous nous ferons un plaisir de vous répondre.
+                </p>
+                
             </div>
         </div>
     </div>
