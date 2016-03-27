09c76b2e60bfb8a9f96f0bb51c190f82e8dfa42c
ealhad <charles.delacombe@gmail.com>
2016-03-28 01:05:16 +0200

    case-sensitive urls fix

--- app/views.py
+++ app/views.py
@@ -10,15 +10,15 @@ def index():
 
 @app.route('/<page>')
 def get_root(page):
-    return render_template(page + '.html')
+    return render_template(page.lower() + '.html')
 
 
 @app.route('/entreprises/<page>')
 def get_entreprises(page):
-    return render_template('entreprises/' + page + '.html')
+    return render_template('entreprises/' + page.lower() + '.html')
 
 
 @app.route('/etudiants/<page>')
 def get_etudiants(page):
     print('searching for', page)
-    return render_template('etudiants/' + page + '.html')
+    return render_template('etudiants/' + page.lower() + '.html')
